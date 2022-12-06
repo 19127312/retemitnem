3b9bba01b79b43f0066ba95b3321b4f97767c43f
luong07734 <le.ky.luong@gmail.com>
2022-12-06 16:05:06 +0700

    handle slides and full screen

--- src/Components/SlidePage/StyledSlideComponent.js
+++ src/Components/SlidePage/StyledSlideComponent.js
@@ -167,6 +167,9 @@ export const StyledButton = styled(Button)`
 export const StyledPrensatationContainer = styled.div`
   position: relative;
   background-color: white;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
   width: 90%;
   height: 90%;
   padding: 20px;
@@ -345,5 +348,33 @@ export const StyledCenterContainer = styled.div`
   flex-direction: column;
   justify-content: center;
   margin: 20px 10px 0px 10px;
+  align-items: center;
+`;
+
+export const StyledLeftRightContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  gap: 200px;
+  margin: 30px 0px;
+`;
+
+export const StyledFullScreenContainer = styled.div`
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  left: 0;
+  top: 0;
+  z-index: 10;
+  overflow: visible;
+  background: lime;
+`;
+
+export const StyledBarContainer = styled.div`
+  height: 90%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
   align-items: center;
 `;
